7b3ce57fe0807bac6eb26b245ed64d7c56226b7d
Blake Neely <blakeneely21@gmail.com>
2019-09-22 23:44:38 -0500

    picture and banner upload

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,22 +1,21 @@
 $(document).ready(function() {
-    $(".navbar-burger").click(function() {
-        $(".navbar-burger").toggleClass("is-active");
-        $(".navbar-menu").toggleClass("is-active");
-    });
-
-    $("#hero-button").on("click", function(){
-        hideSplash();
-        showBody();
-    });
-
     function hideSplash(){
-        $(".hero").addClass("animated zoomOut");
-        setTimeout(function(){$(".hero").hide();}, 500);
+        $("#hero").addClass("animated zoomOut");
+        setTimeout(function(){$("#hero").hide();}, 500);
     };
     function showBody(){
         $(".body").addClass("animated zoomIn");
         setTimeout(function(){$(".body").show();}, 500)
     };
+    $(".navbar-burger").click(function() {
+        $(".navbar-burger").toggleClass("is-active");
+        $(".navbar-menu").toggleClass("is-active");
+    });
+    $("#hero-button").on("click", function(){
+        hideSplash();
+        showBody();
+    });
+
     
 
 });
