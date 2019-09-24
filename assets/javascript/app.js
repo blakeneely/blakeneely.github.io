6972628fb9490ceac579963ed3fd8bc3a893d582
Blake Neely <blakeneely21@gmail.com>
2019-09-24 17:05:01 -0500

    added image modals

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,17 +10,43 @@ $(document).ready(function() {
     };
     function showContactModal(){
         $(".contact-modal").toggleClass("is-active")
+    };
+    function showHuddleModal(){
+        $(".huddle-modal").toggleClass("is-active");
     }
-    $(".contact").on("click", function(){
-        // event.preventDefault;
-        showContactModal();
-    });
+    function showGiftasticModal(){
+        $(".giftastic-modal").toggleClass("is-active");
+    }
+    function showTrainModal(){
+        $(".train-modal").toggleClass("is-active");
+    }
+    function showCrystalModal(){
+        $(".crystal-modal").toggleClass("is-active");
+    }
+    // Modal Event Listeners
+    $("#giftastic-image").on("click", showGiftasticModal);
+    $(".giftastic-delete").on("click", showGiftasticModal);
+
+    $("#train-image").on("click", showTrainModal);
+    $(".train-delete").on("click", showTrainModal);
+
+    $("#crystal-image").on("click", showCrystalModal);
+    $(".crystal-delete").on("click", showCrystalModal);
+
+
+    $("#huddle-image").on("click", showHuddleModal);
+    $(".huddle-delete").on("click", showHuddleModal);
+
+    $(".contact").on("click", showContactModal);
     $(".contact-delete").on("click", showContactModal);
-    
+
+    // Navbar Event Listener
     $(".navbar-burger").click(function() {
         $(".navbar-burger").toggleClass("is-active");
         $(".navbar-menu").toggleClass("is-active");
     });
+
+    // Hero Event Listener
     $("#hero-button").on("click", function(){
         hideSplash();
         showBody();
